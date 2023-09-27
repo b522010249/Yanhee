855a8e6c38ca724121895ce591f5958cd71272f3
b522010249 <ford009@hotmail.com>
2023-09-27 11:19:50 +0700

    first commit

---
+++ windows/Yanhee/MainPage.h
@@ -0,0 +1,19 @@
+﻿#pragma once
+#include "MainPage.g.h"
+#include <winrt/Microsoft.ReactNative.h>
+
+namespace winrt::Yanhee::implementation
+{
+    struct MainPage : MainPageT<MainPage>
+    {
+        MainPage();
+    };
+}
+
+namespace winrt::Yanhee::factory_implementation
+{
+    struct MainPage : MainPageT<MainPage, implementation::MainPage>
+    {
+    };
+}
+
