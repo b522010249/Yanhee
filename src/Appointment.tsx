bb1ee8e75820798944db8e42a91103a1eedeb629
b522010249 <ford009@hotmail.com>
2023-12-04 09:22:50 +0700

    add appointment

--- src/Appointment.tsx
+++ src/Appointment.tsx
@@ -1,15 +1,18 @@
 import { QuerySnapshot, getDocs, collectionGroup } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
-import { Text, View } from 'react-native'
+import { Text, View} from 'react-native'
 import { db } from '../database/config';
+import { Calendar } from 'react-native-calendars';
+import { Picker } from '@react-native-picker/picker';
 
 const Appointment:React.FC = () => {
     const [appointments, setAppointments] = useState<any[]>([]);
+    const [selectedValue, setSelectedValue] = useState('option1');
     useEffect(() => {
         const fetchData = async () => {
           const Collection = collectionGroup(db,'appointment');
           const Snapshot: QuerySnapshot = await getDocs(Collection);
-          const Data = Snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+          const Data = Snapshot.docs.map((doc) => ({ date: doc.id, ...doc.data() }));
           setAppointments(Data);
           console.log(Data)
         };
@@ -18,7 +21,22 @@ const Appointment:React.FC = () => {
       }, []);
   return (
     <View>
-
+      <Picker
+        selectedValue={selectedValue}
+        onValueChange={(itemValue, itemIndex) => setSelectedValue(itemValue)}
+      >
+        <Picker.Item label="Option 1" value="option1" />
+        <Picker.Item label="Option 2" value="option2" />
+        <Picker.Item label="Option 3" value="option3" />
+      </Picker>
+      <Calendar
+      onDayPress={date=>{
+        console.log(date.dateString)
+      }}
+      markingType={'period'}
+      markedDates={{
+        '2023-12-24':{color:'red'}
+      }}/>
     </View>
   )
 }
